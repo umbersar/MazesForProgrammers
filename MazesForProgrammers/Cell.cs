bb0f7df5f79dabc28d0215a820858b3c0a2076dd
Ramneek Singh <ramneek.singh@csiro.au>
2021-01-21 16:06:49 +1100

    Add project files.

---
+++ MazesForProgrammers/Cell.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazesForProgrammers {
+    public class Cell {
+        public int Row { get; private set; }//x co-ordinate of the cell
+        public int Column { get; private set; }//y co-ordinate of the cell
+        public List<Cell> Links { get; private set; }
+        public Cell North { get; set; }
+        public Cell South { get; set; }
+        public Cell East { get; set; }
+        public Cell West { get; set; }
+
+        public Cell(int row, int column) {
+            this.Row = row;
+            this.Column = column;
+            this.Links = new List<Cell>();
+        }
+
+        public void Link(Cell cell, bool biDir = true) {
+            this.Links.Add(cell);
+
+            if (biDir)
+                cell.Link(this, false);
+        }
+
+        public void UnLink(Cell cell, bool biDir = true) {
+            this.Links.Remove(cell);
+
+            if (biDir)
+                cell.UnLink(this, false);
+        }
+
+        public bool IsLinked(Cell cell) {
+            return this.Links.Contains(cell);
+        }
+
+        public List<Cell> Neighbours() {
+            List<Cell> neighbours = new List<Cell>() { North, South, East, West };
+            return neighbours.FindAll(nr => nr != null);
+        }
+    }
+}
---
+++ MazesForProgrammers/Grid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazesForProgrammers {
+    public class Grid {
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public Cell[,] Cells { get; private set; }
+        public Grid(int rows, int columns) {
+            this.Rows = rows;
+            this.Columns = columns;
+            this.Cells = new Cell[rows, columns];
+
+            Initialize();
+        }
+
+        private void Initialize() {
+            Prepare_Grid();
+            Configure_Cells();
+        }
+
+        private void Prepare_Grid() {
+            for (int i = 0; i < Rows; i++) {
+                for (int j = 0; j < Columns; j++) {
+                    Cells[i, j] = new Cell(i, j);
+                }
+            }
+        }
+        private void Configure_Cells() {
+            for (int i = 0; i < Rows; i++) {
+                for (int j = 0; j < Columns; j++) {
+                    Cells[i, j].North = new Cell(i, j);
+                    Cells[i, j].South = new Cell(i, j);
+                    Cells[i, j].East = new Cell(i, j);
+                    Cells[i, j].West = new Cell(i, j);
+                }
+            }
+        }
+
+        private Cell GetCell(int row, int col) {
+            if (row < 0 || col < 0 || row + 1 > Rows || col + 1 > Columns)
+                return null;
+            else
+                return this.Cells[row, col];
+        }
+
+    }
+}
